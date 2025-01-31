3dd9d414c3e85dc9c894fedcb84eeff6703a1453
joemyson <joemyson1402@gmail.com>
2025-01-31 16:12:09 -0300

    iniciado projeto

---
+++ src/main/java/com/jsctech/desafiogestorveiculos/DesafioGestorVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.jsctech.desafiogestorveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGestorVeiculosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioGestorVeiculosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsctech/desafiogestorveiculos/DesafioGestorVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsctech.desafiogestorveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGestorVeiculosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

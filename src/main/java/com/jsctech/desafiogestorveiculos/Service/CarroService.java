9ee05e7a0d455ad0211dc99084b3818f658b955f
joemyson <joemyson1402@gmail.com>
2025-02-01 19:17:02 -0300

    criação das camada Service

---
+++ src/main/java/com/jsctech/desafiogestorveiculos/Service/CarroService.java
@@ -0,0 +1,56 @@
+package com.jsctech.desafiogestorveiculos.Service;
+
+import com.jsctech.desafiogestorveiculos.Entity.Carro;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CarroService {
+
+    private static Map<Long, Carro> listaCarros = new HashMap<>();
+
+    private ResponseEntity<Carro> buscarCarroId(Long id) {
+        Carro carro = listaCarros.get(id);
+        if (carro == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(carro);
+    }
+
+    private List<Carro> buscarTodosCarros() {
+
+        return new ArrayList<>(listaCarros.values());
+    }
+
+    private ResponseEntity<Carro> CadastraCarro(Carro carro) {
+        Carro carroEncontrado = listaCarros.get(carro.getId());
+        if (carroEncontrado == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(carroEncontrado);
+    }
+
+    private ResponseEntity<Carro> AtualizaCarro(Carro carro) {
+        Carro carroEncontrado = listaCarros.get(carro.getId());
+        if (carroEncontrado == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        listaCarros.put(carro.getId(), carro);
+        return ResponseEntity.status(HttpStatus.OK).body(carroEncontrado);
+    }
+
+    private ResponseEntity<String> CadastraCarro(Long id) {
+        Carro carroEncontrado = listaCarros.get(id);
+        if (carroEncontrado == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        listaCarros.remove(id);
+        return ResponseEntity.status(HttpStatus.OK).body("carro removido com sucesso");
+    }
+
+}
---
+++ src/main/java/com/jsctech/desafiogestorveiculos/Service/MotoService.java
@@ -0,0 +1,56 @@
+package com.jsctech.desafiogestorveiculos.Service;
+
+
+import com.jsctech.desafiogestorveiculos.Entity.Moto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MotoService {
+
+    private static Map<Long, Moto> listaMotos = new HashMap<>();
+
+    private ResponseEntity<Moto> buscarMotoId(Long id) {
+        Moto moto = listaMotos.get(id);
+        if (moto == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(moto);
+    }
+
+    private List<Moto> buscarTodasMotos() {
+
+        return new ArrayList<>(listaMotos.values());
+    }
+
+    private ResponseEntity<Moto> CadastraMoto(Moto moto) {
+        Moto motoEncontrado = listaMotos.get(moto.getId());
+        if (motoEncontrado == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(motoEncontrado);
+    }
+
+    private ResponseEntity<Moto> AtualizaMotos(Moto moto) {
+     Moto motoEncontrado = listaMotos.get(moto.getId());
+        if (motoEncontrado == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        listaMotos.put(moto.getId(), moto);
+        return ResponseEntity.status(HttpStatus.OK).body(motoEncontrado);
+    }
+
+    private ResponseEntity<String> CadastraMoto(Long id) {
+       Moto motoEncontrado = listaMotos.get(id);
+        if (motoEncontrado == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        listaMotos.remove(id);
+        return ResponseEntity.status(HttpStatus.OK).body("moto removido com sucesso");
+    }
+
+}
